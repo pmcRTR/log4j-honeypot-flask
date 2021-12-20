ae4d6592069b5386c0ba143fb184db0f6904eb0d
pmcRTR <pmc.retro@startmail.com>
2021-12-20 11:52:57 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,13 +3,6 @@ import requests, urllib.request
 import json
 import os
 
-#### Set your Slack or Teams or Mattermost webhook here, or in environment variable WEBHOOK_URL ####
-webhook_url = ""
-# For help setting up the webhook, see:
-# Slack: https://api.slack.com/messaging/webhooks
-# Teams: https://docs.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook
-# Mattermost: https://docs.mattermost.com/developer/webhooks-incoming.html
-
 #### Set the name of this honeypot instance here, or in environment variable HONEYPOT_NAME ####
 # (use a descriptive name so you know when alerts come in where they were triggered)
 honeypot_name = "My log4j honeypot"
@@ -20,9 +13,6 @@ honeypot_port = 8080
 
 if "HONEYPOT_NAME" in os.environ and os.environ["HONEYPOT_NAME"].strip() != "":
     honeypot_name = os.environ["HONEYPOT_NAME"]
-
-if "WEBHOOK_URL" in os.environ and os.environ["WEBHOOK_URL"].strip() != "":
-    webhook_url = os.environ["WEBHOOK_URL"].strip()
 
 if "HONEYPOT_PORT" in os.environ and os.environ["HONEYPOT_PORT"].strip() != "":
     try:
@@ -36,33 +26,29 @@ app = Flask(__name__)
 
 def reportHit(request):
     msglines = []
-    msglines.append("Alert from log4j honeypot " + honeypot_name)
-    msglines.append("Suspicious request received from IP: "+ request.remote_addr)
-    msglines.append("Review HTTP headers for payloads:")
-    msglines.append("```")
+    msglines.append('{"honeypot": "' + honeypot_name + '", "attacker_ip": "' + request.remote_addr + '"')
     for header in request.headers:
         msglines.append(str(header))
     for fieldname, value in request.form.items():
         msglines.append(str((fieldname, value)))
-    msglines.append("```")
-
     msg = {'text':'\n '.join(msglines)}
-    response = requests.post(
-        webhook_url, data=json.dumps(msg),
-        headers={'Content-Type': 'application/json'},
-        proxies=urllib.request.getproxies(),
-    )
-    if response.status_code != 200:
-        print('Request to webhook returned an error %s, the response is:\n%s' % (response.status_code, response.text))
+    jsonlines = json.dumps(msg)
+    print(jsonlines)
+
+#    response = requests.post(
+#        webhook_url, data=json.dumps(msg),
+#        headers={'Content-Type': 'application/json'},
+#        proxies=urllib.request.getproxies(),
+#    )
+#    if response.status_code != 200:
+#        print('Request to webhook returned an error %s, the response is:\n%s' % (response.status_code, response.text))
 
 login_form = """<html>
-<head><title>Secure Area Login</title></head>
+<head><title>Transaction</title></head>
 <body>
-<h1>Log in to Secure Area</h1>
 <form method='post' action='/'>
-  <b>Username:</b> <input name='username' type='text'/><br/>
-  <b>Password:</b> <input name='password' type='password'/><br/>
-  <input type='submit' name='submit'/>
+  <input name='transaction' type='text' placeholder='transaction'/>
+  <input type='submit' name='submit' value='Send'/>
 </form>
 </body></html>"""
 
@@ -79,14 +65,10 @@ def homepage(hostname="NA"):
             print(value)
             if "${" in value:
                 reportHit(request)
-        return("<html><head><title>Login Failed</title></head><body><h1>Login Failed</h1><br/><a href='/'>Try again</a></body></html>")
+        return(login_form)
     else:
         return(login_form)
 
 
 if __name__ == '__main__':
-    if not webhook_url:
-        print("ERROR: WEBHOOK_URL environment variable not set! I will not be able to report exploit attempts!")
-        print("For Docker, use -e WEBHOOK_URL=xxxxx or for shell use export WEBHOOK_URL=xxxxx")
-    else:
-        app.run(debug=False, host='0.0.0.0', port=honeypot_port)
+    app.run(debug=False, host='0.0.0.0', port=honeypot_port)
